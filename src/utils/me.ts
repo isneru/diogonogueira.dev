f9f5e9468f253b8d77046802a5185afae0ce4f3a
Diogo Nogueira <diogoclanogueira@gmail.com>
2023-08-16 11:23:20 +0100

    SSR Added, dynamic SEO Images

---
+++ src/utils/me.ts
@@ -0,0 +1,77 @@
+export const internships = [
+  {
+    where: "Computer repair & service",
+    when: "01/2022 - 05/2022",
+    location: "Rethymno, Greece",
+    tasks: [
+      "Assistance in computers' formats and operational systems upgrades",
+      "Computers installation and set up in a school environment",
+      "Wi-Fi network installations",
+      "Camera services and repairs",
+      "Computer services and repairs",
+      "General assistance to my supervisor",
+      "Acquisition of knowledge in the field of IT and electronics"
+    ]
+  },
+  {
+    where: "ITSector",
+    when: "04/2021 - 07/2021",
+    location: "Porto, Portugal",
+    tasks: [
+      "Linked computers to certain networks and peripheral equipment, including printers and scanners",
+      "Patched software and installed new versions to eliminate security vulnerabilities and protect data",
+      "Helped streamline repair processes and update procedures for support action consistency",
+      "Configured hardware, devices and software to set up workstations for employees"
+    ]
+  }
+]
+
+export const education = [
+  {
+    title: "College",
+    where: "Escola Superior de Tecnologia e Gestão, Felgueiras",
+    before: "Computer Security and Computer Networks",
+    after: "10/2022 - Present"
+  },
+  {
+    title: "High School",
+    where: "Instituto Profissional de Tecnologias Avançadas, Porto",
+    before: "Hardware and Software Management",
+    after: "09/2018 - 07/2021"
+  }
+]
+
+export const career = [
+  {
+    title: "Current Position",
+    where: "CeX - Computer Entertainment Exchange",
+    before: "Portugal",
+    after: "06/2023 - Present"
+  }
+]
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Node.js",
+  "Express.js",
+  "React",
+  "TypeScript",
+  "Next.js",
+  "MongoDB",
+  "SQL",
+  "Prisma",
+  "TailwindCSS",
+  "Git",
+  "GitHub",
+  "Svelte",
+  "SvelteKit",
+  "Astro",
+  "C",
+  "PHP",
+  "Java",
+  "VSCode",
+  "Figma",
+  "Adobe Photoshop"
+]
