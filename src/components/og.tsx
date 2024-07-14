0f8405068632cb93d434691478bfb3a2aaff3314
Diogo Nogueira <diogoclanogueira@gmail.com>
2024-07-15 00:34:56 +0100

    test

--- src/components/og.tsx
+++ src/components/og.tsx
@@ -7,30 +7,39 @@ type Props = {
 export const OG = ({ pathname }: Props) => {
 	return (
 		<>
-			<meta property='og:url' content={variants[pathname].website} />
+			<meta property='og:url' content={variants[pathname ?? '/'].website} />
 			<meta property='og:type' content='website' />
-			<meta property='og:title' content={variants[pathname].title} />
+			<meta property='og:title' content={variants[pathname ?? '/'].title} />
 			<meta
 				property='og:description'
-				content={variants[pathname].description}
+				content={variants[pathname ?? '/'].description}
 			/>
-			<meta property='og:image' content={variants[pathname].image} />
+			<meta property='og:image' content={variants[pathname ?? '/'].image} />
 
 			<meta name='robots' content='nofollow' />
-			<meta name='keywords' content={variants[pathname].keywords} />
-			<meta name='description' content={variants[pathname].description} />
+			<meta name='keywords' content={variants[pathname ?? '/'].keywords} />
+			<meta
+				name='description'
+				content={variants[pathname ?? '/'].description}
+			/>
 
 			<meta name='twitter:card' content='summary_large_image' />
 			<meta property='twitter:domain' content='diogonogueira.dev' />
-			<meta property='twitter:url' content={variants[pathname].website} />
-			<meta name='twitter:title' content={variants[pathname].title} />
+			<meta
+				property='twitter:url'
+				content={variants[pathname ?? '/'].website}
+			/>
+			<meta name='twitter:title' content={variants[pathname ?? '/'].title} />
 			<meta
 				name='twitter:description'
-				content={variants[pathname].description}
+				content={variants[pathname ?? '/'].description}
 			/>
-			<meta name='twitter:image' content={variants[pathname].image} />
-			<title>{variants[pathname].title}</title>
-			<meta name='description' content={variants[pathname].description} />
+			<meta name='twitter:image' content={variants[pathname ?? '/'].image} />
+			<title>{variants[pathname ?? '/'].title}</title>
+			<meta
+				name='description'
+				content={variants[pathname ?? '/'].description}
+			/>
 		</>
 	)
 }
