a3629674565eecc265e572becb02ae5d862d22fc
isneru <diogoclanogueira@gmail.com>
2025-07-12 02:10:57 +0100

    whoops

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
 	plugins: [tailwindcss(), solidPlugin()],
 	server: {
-		port: 3000,
-		allowedHosts: ['tools-troy-palm-associates.trycloudflare.com']
+		port: 3000
 	},
 	build: {
 		target: 'esnext'
